da922a1822b76f1c9f169dab26b050af9fead477
Suprina Tamrakar <tamrakar.suprina@gmail.com>
2023-01-09 15:46:47 +0545

    Create Patient Component

---
+++ front-end/src/Components/create-patient.component.js
@@ -0,0 +1,43 @@
+// CreatPatient Component for add new patient
+
+// Import Modules
+import React, { useState } from "react";
+import axios from "axios";
+import PatientForm from "./PatientForm";
+
+//CreatePatient Component
+const CreatePatient = () => {
+  const [formValues] = useState({
+    name: '',
+    gender: '',
+    phoneNumber: '',
+    zipCode: '',
+    streetAddress: '',
+    city: '',
+    birthday: '',
+    email: '',
+    lastAppointment: '',
+    nextAppointment: '',
+  });
+  const onSubmit = (patientObject) => {
+    axios
+      .post("http://localhost:4000/patient/create-patient", patientObject)
+      .then((response) => {
+        if (response.status === 200) alert("Patient successfully created");
+        else Promise.reject();
+      })
+      .catch((err) => alert("Something went wrong"));
+  };
+
+  return (
+    <PatientForm
+      initialValues={formValues}
+      onSubmit={onSubmit}
+      enableReinitialize
+    >
+      Create Patient
+    </PatientForm>
+  );
+};
+
+export default CreatePatient;
